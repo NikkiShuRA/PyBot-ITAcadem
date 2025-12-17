e63a992d2cd91a64185e2f9fb0ae35a719992b53
NikkiShuRA <xak-bag-303@yandex.ru>
2025-12-17 18:13:54 +0500

    Удаление старых импортов из user_module

--- src/pybot/db/models/user_module/__init__.py
+++ src/pybot/db/models/user_module/__init__.py
@@ -1,27 +1,10 @@
 from ...base_class import Base
 
 from .user import User
-from .user_activity_status import UserActivityStatus
-from .admin_role import AdminRole
-from .academic_role import AcademicRole
-from .level import Level
-from .competence import Competence
-from .user_competence import UserCompetence
-from .user_achievement import UserAchievement
-from .valuation import Valuation
-from .user_level import UserLevel
+from .user_activity import UserActivity
 
 __all__ = [
     "Base",
     "User",
-    "UserActivityStatus",
-    "AdminRole",
-    "AcademicRole",
-    "Level",
-    "Competence",
-    "UserCompetence",
-    "UserAchievement",
-    "UserTask",
-    "UserLevel",
-    "Valuation",
+    "UserActivity",
 ]
