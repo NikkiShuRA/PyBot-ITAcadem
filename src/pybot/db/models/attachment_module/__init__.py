b57bd74f8e444b8f592e86e8896ff85c3567f312
NikkiShuRA <xak-bag-303@yandex.ru>
2026-01-03 02:04:24 +0500

    Исправление импортов

---
+++ src/pybot/db/models/attachment_module/__init__.py
@@ -0,0 +1,10 @@
+# from ...base_class import Base
+
+# from .attachments import Attachment
+# from .attachment_types import AttachmentType
+
+# __all__ = [
+#     "Base",
+#     "Attachment",
+#     "AttachmentType"
+# ]
