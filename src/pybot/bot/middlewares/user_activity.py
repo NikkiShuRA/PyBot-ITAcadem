b8f2e542e42c80671628cbab6c175b181a71f88c
Cewerty <88933231+Cewerty@users.noreply.github.com>
2026-02-04 04:21:22 +0500

    fix: исправил привязку telegram id к internal id и его использования для проверки роли

--- src/pybot/bot/middlewares/user_activity.py
+++ src/pybot/bot/middlewares/user_activity.py
@@ -19,36 +19,30 @@ class UserActivityMiddleware(BaseMiddleware):
         event: TelegramObject,
         data: dict[str, Any],
     ) -> Any:
-        # 1. 🚀 Сначала выполняем бизнес-логику бота (хендлеры)
-        # Пользователь мгновенно получает ответ, не ожидая БД.
-        result = await handler(event, data)
-
         # 2. 🕵️‍♂️ Фоновая логика трекинга (выполняется после ответа)
         user = data.get("event_from_user")
         if not user:
-            return result
+            return await handler(event, data)
 
         container = data.get(CONTAINER_NAME)
         if not container:
             # Логируем ошибку, но не ломаем работу бота, результат возвращаем
             logger.error("❌ Dishka container not found in data!")
-            return result
+            return await handler(event, data)
 
         try:
             async with container() as request_container:
                 db = await request_container.get(AsyncSession)
                 repo: UserRepository = await request_container.get(UserRepository)
-
-                # Выполняем "слепой" апдейт
-                await repo.update_user_last_active(db=db, user_id=user.id)
-                data["user_id"] = user.id  # Добавляем user_id для отвязки логики в сервисах от telegram id
-                data["user_roles"] = set(await repo.get_user_roles(db=db, user_id=user.id))
-                # Коммитим. Если изменения были - они сохранятся.
-                # Если 5 минут не прошло - SQL вернет 0 rows, коммит будет пустым (быстро).
-                await db.commit()
+                db_user = await repo.get_user_by_telegram_id(db, tg_id=user.id)
+                if db_user:
+                    await repo.update_user_last_active(db=db, user_id=db_user.id)
+                    data["user_id"] = db_user.id  # Добавляем user_id для отвязки логики в сервисах от telegram id
+                    data["user_roles"] = set(await repo.get_user_roles(db=db, user_id=db_user.id))
+                    await db.commit()
 
         except Exception:
             logger.exception("Failed to update user activity")
             raise
 
-        return result
+        return await handler(event, data)
