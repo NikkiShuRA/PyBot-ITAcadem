8c16d8aaae571b437283c48b6602ad215d9837ec
Cewerty <88933231+Cewerty@users.noreply.github.com>
2026-02-04 17:45:30 +0500

    fix: исправил логику проверки ролей, добавив обрабатывать множество ролей на одном элементе (#56)
    * Исправил ошибку маппинга c UserEntity, добавил нормализацию для номера телефона в DTO при создании пользователя, добавил мапперов для перевода из ORM моделей в доменные объекты
    
    * Добавил Value object для Points, mapper для преобразования пользователя в dto, отрефакторил логику регистрации, добавив валидацию вводимых значений Ф.И.О., исправил логику зачисления баллов
    
    * Добавление недостающих зависимостей в pyproject.toml
    
    * Удаление лишнего файла
    
    * Добавил Makefile для группировки команд для инструментов, добавил CI-pipeline, через Github Action, используя Makefile, для улучшения качества кода и повышения DX
    
    * Добавил недостающие инструменты в dev-зависимости необходимые для CI-pipeline-а
    
    * Убрал бесполезную директиву type: ignore в config.py
    
    * Использование официального uv Action-а от Astral для ускорения его установки
    
    * feat/добавление архитектурного манифеста и описания workflow проекта
    
    * docs/Изменил название workflow на то, которое автоматически распознаётся Github
    
    * docs/Использовал markdown linter на ADR-ах
    
    * docs/Добавил шаблоны для issue и pull request-ов
    
    * docs/дополнение и актуализации документации к проекту, создание нового ADR, актуализация README
    
    * feat: добавил фильтер для conditional UI в aiogram-dialog, с добавлением списка ролей пользователя в данные из middleware
    
    * refactor: добавил VO в объекты кода, и инструменты в CI-pipeline
    
    * chore: добавил свою dev-ветку в код Github Actions для того, чтобы проверить его работу
    
    * chore: изменил конфигурацию Realise pipeline-а
    
    * ci: исправление ошибок конфигурации в Github Action
    
    * ci: дальнейшее исправление конфигурации pipeline-а
    
    * fix: убрал enqueue=True из логгера для исправления блокировки программы при её завершении
    
    * fix: исправил логику проверки ролей, добавив возможность эффективного обрабатывать множество ролей на одном хэндлере, диалоге

--- run.py
+++ run.py
@@ -1,6 +1,10 @@
 import asyncio
 
 from src.pybot.bot.tg_bot_run import tg_bot_main
+from src.pybot.core import logger
 
 if __name__ == "__main__":
-    asyncio.run(tg_bot_main())
+    try:
+        asyncio.run(tg_bot_main())
+    finally:
+        logger.complete()
--- src/pybot/utils/__init__.py
+++ src/pybot/utils/__init__.py
@@ -1,1 +1,2 @@
 from .phonenumber_normalization import normalize_phone as normalize_phone
+from .role_checking import has_any_role
