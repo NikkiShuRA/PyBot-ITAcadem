dee457f35546dbe4f722df3f55ea86053e784150
Cewerty <88933231+Cewerty@users.noreply.github.com>
2026-03-12 00:24:35 +0500

    docs: добавил комментарии для будущего рефакторинга dto

--- src/pybot/dto/level_dto.py
+++ src/pybot/dto/level_dto.py
@@ -8,6 +8,7 @@ class LevelReadDTO(BaseDTO):
     DTO для отображения данных уровня.
     """
 
+    # TODO Проанализировать необходимость этого параметра, учитывая его дублирование в связанном DTO
     system: LevelTypeEnum
     name: str
     required_points: Points
